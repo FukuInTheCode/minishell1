5afd005695468488e97e868a8ca3aac906c3653c
Tom C. <tom.carlier@epitech.eu>
2024-02-10 23:15:16 +0100

    [fix] Fix the my_getenv function

--- src/my_getenv.c
+++ src/my_getenv.c
@@ -14,7 +14,7 @@ char *my_getenv(char const *name)
     size_t name_len = my_strlen(name);
 
     for (int i = 0; environ[i]; i++) {
-        if (my_strncmp(environ, name, name_len))
+        if (my_strncmp(environ[i], name, name_len))
             continue;
         return environ[i] + name_len + 1;
     }
