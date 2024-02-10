cdfc333d527debca82a729f2b49102b7bda9c665
Tom C. <tom.carlier@epitech.eu>
2024-02-10 23:56:19 +0100

    [feat] Add the my_strdup and so the my_strcpy

--- include/my.h
+++ include/my.h
@@ -19,13 +19,19 @@
     #include <signal.h>
     #include <stdbool.h>
     #include <string.h>
+    #include <sys/types.h>
+    #include <sys/stat.h>
+    #include <fcntl.h>
 
 size_t my_strlen(char const *);
 char *my_memset(char *, char, size_t buf_len);
 char *my_strcat(char *, char const *);
 int my_strncmp(char const *, char const *, int);
+char *my_strcpy(char *, char const *);
+char *my_strdup(char const *);
 
 char *my_getenv(char const *);
+bool cmd_exist(char const *);
 int my_shell(void);
 
 #endif
