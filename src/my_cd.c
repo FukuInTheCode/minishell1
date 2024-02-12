d126170e2a3a25e23a9a28bc2c0f97e191f0620e
Tom C <tom.carlier@epitech.eu>
2024-02-12 22:02:23 +0100

    [fix] Rienforce the erro handling in the cd

--- src/my_cd.c
+++ src/my_cd.c
@@ -17,7 +17,7 @@ int my_cd(char *argv[])
         path = my_getenv("HOME");
     if (argv[1] && !my_strcmp("-", argv[1]))
         path = my_getenv("OLDPWD");
-    if (!path)
+    if (!path || access(path, F_OK))
         return 1;
     lstat(path, &s);
     if (!S_ISDIR(s.st_mode))
