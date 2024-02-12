485811dc165a8c77de8e872e98ae510d8325809d
Tom C <tom.carlier@epitech.eu>
2024-02-12 22:48:16 +0100

    [feat] Add a do_exit function that check if the exit command it valid

---
+++ src/my_exit.c
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2024
+** my_exit.c
+** File description:
+** my_exit.c
+*/
+
+#include "my.h"
+
+bool do_exit(char *argv[], int *ret)
+{
+    int i = 0;
+
+    if (!argv[1])
+        return true;
+    if (argv[2])
+        return false;
+    for (; argv[1][i] && ('0' <= argv[1][i] && argv[1][i] <= '9'); i++);
+    if (argv[1][i])
+        return false;
+    *ret = my_getnbr(argv[1]);
+    return true;
+}
